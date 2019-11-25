87f7453a24f2a509817e810d79318207976726fe
Matt Holmes <holmesm8@gmail.com>
2019-11-24 23:10:57 -0700

    Update method in card_generator file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,11 +1,32 @@
+# card class is already created, pull it in here with require
+require './lib/card'
+
 class CardGenerator
+  attr_reader :cards
 
   def initialize(file)
     @file = file
-  end
+    @cards = []
 
+# get info from file and split it into Cards
+    file = File.open("cards.txt", "r")
+    file_data = file.readlines.map(&:chomp)
+    file.close
 
+    def get_file_as_string(filename)
+      data = ''
+      f = File.open(filename, "r")
+      f.each_line do |line|
+        data += line
+      end
+      return data
+    end
 
+    # cards = []
+    # file_data.each do |line|
+    #   line.to_s
+    #
+    #   @cards << line
 
-
+    end
 end
